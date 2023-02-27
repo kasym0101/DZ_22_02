19f62b1b67ed7bb895ec24549abda2c8d6eca560
Kasym0101 <kasym.dzhorobaev@gmail.com>
2023-02-27 19:57:56 +0600

    first commit

---
+++ bin/dz_22_02.dart
@@ -0,0 +1,20 @@
+import 'package:dz_22_02/dz_22_02.dart' as dz_22_02;
+
+void main() {
+  double b = 4.2;
+  int a = 3;
+  print(b + a);
+}
+
+// void main() {
+//   String text = "Name";
+//   text = "Flutter";
+//   print("I want to learn " + text);
+// }
+
+// void main() {
+//   double myNumber = 2.225;
+//   String myText;
+//   myText = myNumber.toString();
+//   print(myText);
+// }
